1323393bda74b82a5d6cc968fac0b07f65b2e407
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-11 14:54:59 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,8 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each {|person, contact_details_hash|
-    binding.pry
+    contact_details_hash.each {|attribute,data|
+      binding.pry
+    }
   }
 end
